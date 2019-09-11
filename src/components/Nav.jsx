c2b68e2c71f638add6975dbc49a9af5a4def8d77
volkov <volkov@vologda.softlab.ru>
2019-09-11 16:58:24 +0300

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+    <div className='header'>
+        <div className='headerLogo'>
+            <img src='https://www.freepnglogos.com/uploads/batman-logo-images-pictures-10.png'/>
+        </div>
+        <div className='pageTitle'>Header</div>
+    </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <div className='nav'>Navigation</div>
+    )
+}
+
+export default Nav;
